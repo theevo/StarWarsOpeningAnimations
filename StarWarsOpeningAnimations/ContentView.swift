08c2c63220a8f26d77281e9bf0140a03632f2cc2
Theo Vora <theo.vora@gmail.com>
2025-03-13 14:12:48 -0500

    Initial Commit

---
+++ StarWarsOpeningAnimations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StarWarsOpeningAnimations
+//
+//  Created by Tana Vora on 3/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StarWarsOpeningAnimations/StarWarsOpeningAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsOpeningAnimationsApp.swift
+//  StarWarsOpeningAnimations
+//
+//  Created by Tana Vora on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsOpeningAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
