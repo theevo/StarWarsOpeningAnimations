573c2916d00a917e0d496780a49c8fb56350aa12
Theo Vora <theo.vora@gmail.com>
2025-08-19 09:59:37 -0500

    randomize the play time

--- StarWarsOpeningAnimationsTests/MusicTests/MusicPlayerTests.swift
+++ StarWarsOpeningAnimationsTests/MusicTests/MusicPlayerTests.swift
@@ -5,11 +5,12 @@
 //  Created by Tana Vora on 8/15/25.
 //
 
+import Foundation
 import Testing
 
 protocol MusicPlayerAdapter {
     func pause()
-    func play()
+    func play() async
 }
 
 class MusicPlayerSpy: MusicPlayerAdapter {
@@ -19,7 +20,9 @@ class MusicPlayerSpy: MusicPlayerAdapter {
         isPlaying = false
     }
     
-    func play() {
+    func play() async {
+        let random = Int.random(in: 0..<3)
+        try! await Task.sleep(for: .seconds(random))
         isPlaying = true
     }
 }
@@ -32,14 +35,29 @@ struct MusicPlayerTests {
     
     @Test func test_play_isPlaying() async throws {
         let sut = MusicPlayerSpy()
-        sut.play()
+        await playTimed(sut: sut)
         #expect(sut.isPlaying == true)
     }
     
     @Test func test_playThenPause_isNotPlaying() async throws {
         let sut = MusicPlayerSpy()
-        sut.play()
+        await playTimed(sut: sut)
         sut.pause()
         #expect(sut.isPlaying == false)
+    }
+    
+    private func playTimed(sut: MusicPlayerAdapter) async {
+        let start = Date()
+        print("▶️ Playing music...")
+        await sut.play()
+        let end = Date()
+        let duration = end.timeIntervalSince(start)
+        print("▶️ ... play took \(duration.inSeconds)")
+    }
+}
+
+extension TimeInterval {
+    var inSeconds: String {
+        String(format: "%.4f seconds", self)
     }
 }
