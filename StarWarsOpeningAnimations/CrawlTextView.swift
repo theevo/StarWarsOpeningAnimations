97fa6392ec32ee2302249c96ebe1f6103af1ef9f
Theo Vora <theo.vora@gmail.com>
2025-08-26 15:44:10 -0500

    create the CrawlTextViewModel from a StarWarsCrawl

--- StarWarsOpeningAnimations/CrawlTextView.swift
+++ StarWarsOpeningAnimations/CrawlTextView.swift
@@ -7,16 +7,20 @@
 
 import SwiftUI
 
+struct CrawlTextViewModel {
+    let text1Centered: String
+    let text2Centered: String
+    let text3Justified: String
+    
+    init(crawl: StarWarsCrawl) {
+        self.text1Centered = crawl.episodeNumber
+        self.text2Centered = crawl.title
+        self.text3Justified = crawl.crawl
+    }
+}
+
 struct CrawlTextView: View {
-    var text1Centered = "Episode IV"
-    var text2Centered = "A NEW HOPE"
-    var text3Justified = """
-    It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire.
-
-    During the battle, Rebel spies managed to steal secret plans to the Empire's ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet.
-
-    Pursued by the Empire's sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy...
-    """
+    let viewModel: CrawlTextViewModel
     
     var uiColor: UIColor = .yellow
     var fontSize: CGFloat = 24
@@ -28,16 +32,16 @@ struct CrawlTextView: View {
     
     var body: some View {
         VStack {
-            Text(text1Centered + "\n")
+            Text(viewModel.text1Centered + "\n")
                 .multilineTextAlignment(.leading)
                 .foregroundStyle(shapeColor)
                 .font(.system(size: fontSize, weight: .heavy))
-            Text(text2Centered)
+            Text(viewModel.text2Centered)
                 .multilineTextAlignment(.leading)
                 .foregroundStyle(shapeColor)
                 .font(.system(size: fontSize, weight: .heavy))
             JustifiedText(
-                text: text3Justified,
+                text: viewModel.text3Justified,
                 font: UIFont.systemFont(ofSize: fontSize, weight: .heavy),
                 color: uiColor
             )
@@ -52,7 +56,10 @@ struct CrawlTextView: View {
     Text("Degrees: \(degrees, specifier: "%.2f")")
     ZStack {
         Color.black.ignoresSafeArea()
-        CrawlTextView(degrees: $degrees)
+        CrawlTextView(
+            viewModel: CrawlTextViewModel(crawl: StarWarsCrawl.Episode4),
+            degrees: $degrees
+        )
     }
     Slider(value: $degrees, in: 0...90)
 }
